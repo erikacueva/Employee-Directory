3d329e8d17e05eea6be5eb85c298a605cd639ed9
erika cueva <ecueva88@gmail.com>
2021-02-25 19:06:30 -0800

    deleted extraneous readme

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header'; 
+import Header from './components/Header'; 
 import EmployeeInfo from "./components/EmployeeInfo";
 
 function App() {
