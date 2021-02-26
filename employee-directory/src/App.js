b51cab8a98d685c5dda869178c48168384a071b8
erika cueva <ecueva88@gmail.com>
2021-02-25 21:55:10 -0800

    updated table js

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'; 
-import EmployeeInfo from "./components/EmployeeInfo";
+// import EmployeeInfo from "./components/EmployeeInfo";
+import Table from ".components/Table";
 
 function App() {
 
   return (
     <>
     <Header/>
+    <Table/>
     <div className="container">
       test
-      {/* <EmployeeInfo/> */}
     </div>
     
     
--- employee-directory/src/components/Table/index.js
+++ employee-directory/src/components/Table/index.js
@@ -1,54 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Table extends Component {
-   constructor(props) {
-      super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
-      this.state = { 
-         employess: [
-            { id: 1, image: "image", name: 'gustav', phone: 21, email: 'email@email.com', dob: "dob" },
-         ]
-      }
-   }
+  constructor(props) {
+    super(props); //since we are extending class Table so we have to use super in order to override Component class constructor
+    this.state = {
+      employess: [
+        {
+          id: 1,
+          image: "image",
+          name: "gustav",
+          phone: 21,
+          email: "email@email.com",
+          dob: "dob",
+        },
+      ],
+    };
+  }
 
-   renderTableHeader() {
-    let header = Object.keys(this.state.employees[0])
+  renderTableHeader() {
+    let header = Object.keys(this.state.employees[0]);
     return header.map((key, index) => {
-       return <th key={index}>{key.toUpperCase()}</th>
-    })
- }
+      return <th key={index}>{key.toUpperCase()}</th>;
+    });
+  }
 
- renderTableData() {
+  renderTableData() {
     return this.state.employees.map((employee, index) => {
-       const { id, image, name, phone, email, dob } = employee //destructuring
-       return (
-          <tr key={id}>
-             <td>{id}</td>
-             <td>{image}</td>
-             <td>{name}</td>
-             <td>{phone}</td>
-             <td>{email}</td>
-             <td>{dob}</td>
-          </tr>
-       )
-    })
- }
+      const { id, image, name, phone, email, dob } = employee; //destructuring
+      return (
+        <tr key={id}>
+          <td>{id}</td>
+          <td>{image}</td>
+          <td>{name}</td>
+          <td>{phone}</td>
+          <td>{email}</td>
+          <td>{dob}</td>
+        </tr>
+      );
+    });
+  }
 
-
- render() {
+  render() {
     return (
-       <div>
-          <h1 id='title'>Employee Directory</h1>
-          <table id='employees'>
-             <tbody>
-                <tr>{this.renderTableHeader()}</tr>
-                {this.renderTableData()}
-             </tbody>
-          </table>
-       </div>
-    )
- }
+      <div>
+        <h1 id="title">Employee Directory</h1>
+        <table id="employees">
+          <tbody>
+            <tr>{this.renderTableHeader()}</tr>
+            {this.renderTableData()}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
-
-
-export default Table //exporting a component make it reusable and this is the beauty of react
+export default Table; //exporting a component make it reusable and this is the beauty of react
